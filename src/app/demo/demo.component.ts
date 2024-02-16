53cb35c97027e5d5f5a513496427cb0911d47bce
Vlaimir Korba <krissky1986@gmail.com>
2024-02-16 21:11:09 +0200

    43 - ngAfterContentChecked Lifecycle Hook | Lifecycle Hooks in Angular

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -9,6 +9,7 @@ import {
   DoCheck,
   AfterContentInit,
   ContentChild,
+  AfterContentChecked,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -20,36 +21,40 @@ import { CommonModule } from '@angular/common';
   styleUrl: './demo.component.scss',
 })
 export class DemoComponent
-  implements OnChanges, OnInit, DoCheck, AfterContentInit
+  implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked
 {
   title: string = 'Demo Component';
   @Input() message: string;
   @ViewChild('temp') tempPara: ElementRef;
   @ContentChild('temp') paraContent: ElementRef;
   constructor() {
-    console.log('Demo component constructor called');
+    // console.log('Demo component constructor called');
     // console.log(this.title);
     // console.log(this.message);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('ngOnChanges Hook called');
+    // console.log('ngOnChanges Hook called');
     // console.log(changes);
   }
 
   ngOnInit() {
-    console.log('ngOnInit Hook called');
+    // console.log('ngOnInit Hook called');
     // console.log(this.tempPara.nativeElement.innerHTML);
   }
 
   ngDoCheck() {
     console.log('DoCheck Hook called');
     // console.log(this.tempPara.nativeElement.innerHTML);
-    console.log('In ngDoCheck', this.paraContent);
+    // console.log('In ngDoCheck', this.paraContent);
   }
   ngAfterContentInit() {
     console.log('ngAfterContentInit Hook called');
     // console.log(this.tempPara.nativeElement.innerHTML);
-    console.log('In ngAfterContentInit', this.paraContent.nativeElement);
+    // console.log('In ngAfterContentInit', this.paraContent.nativeElement);
+  }
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked Hook called');
+    console.log('In ngAfterContentChecked', this.paraContent.nativeElement);
   }
 }
